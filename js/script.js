f57d3c1ed477963c2bb80458491c20a87899cab2
nikita <nikvladimirov8@gmail.com>
2021-09-05 18:38:45 +0300

    major changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,47 @@
-$('document').ready(() =>{
-    console.log('ready')
+var errorMessages = {
+    checkEmail: 'Неверный формат email',
+    userExists: 'Пользователь с таким email уже существует',
+    passwordCompare: 'Пароли не совпадают'
+}
+
+function addErrorMessage(message) {
+    if($('#registration .message-error').length) {
+        $('#registration .message-error').remove()
+    }
+    $('#registration').prepend(
+        '<div class="message-error text-center p-2">' + message +'</div>'
+    )
+}
+
+
+$(function() {
+    $('#registration').submit(function (e) {
+        e.preventDefault()
+        let form = document.getElementById('registration');
+        let formData = new FormData(form)
+        $.ajax({
+            type: 'post',
+            url: 'registration.php',
+            processData: false,
+            contentType : false,
+            data: formData,
+            success: function(response) {
+                console.log(response)
+                let json = JSON.parse(response)
+                console.log(json)
+                if (!json.checkEmail) {
+                    addErrorMessage(errorMessages.checkEmail)
+                }
+                else if (json.resultUser) {
+                    addErrorMessage(errorMessages.userExists)
+                } 
+                else if (json.resultPassword != 0) {
+                    addErrorMessage(errorMessages.passwordCompare)
+                } else {
+                    $('.registration-card').hide()
+                    $('.success-card').show()
+                }
+            }
+        })
+    })
 })
--- registration.php
+++ registration.php
@@ -1,3 +1,48 @@
-<?
+<?php
+
+$users = [
+    ['email' => 'ivan@mail.ru', 'name' => 'Иван', 'id' => '1'],
+    ['email' => 'julia@mail.ru', 'name' => 'Юлия', 'id' => '2'],
+    ['email' => 'mikhail@mail.ru', 'name' => 'Михаил', 'id' => '3'],
+];
+
+if(isset($_POST['email']) && $_POST['email'] &&
+    isset($_POST['password']) && $_POST['password'] &&
+    isset($_POST['passwordRepeat']) && $_POST['passwordRepeat']){
+    $checkEmail = checkEmail($_POST['email']);
+    $resultUser = findUser($_POST['email']);
+    $passwordCompare = passwordCompare($_POST['password'], $_POST['passwordRepeat']);
+    echo json_encode(array(
+        'checkEmail' => $checkEmail,
+        'resultUser' => $resultUser,
+        'resultPassword' => $passwordCompare
+    ));
+} 
+
+function messageLog ($message) {
+    $logMessage = date('Y-m-d H:i:s') . ' ' . $message;
+    file_put_contents(__DIR__ . '/logs/log.txt', $logMessage . PHP_EOL, FILE_APPEND);
+}
+
+function checkEmail($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+function findUser($email) {
+    global $users;
+    foreach($users as $user) {
+        if($email == $user['email']){
+            messageLog("E-mail адрес '{$email}' уже существует.\n");
+            return true;
+        } 
+    }
+    messageLog("E-mail адрес '{$email}' не существует.\n");
+    return false;
+}
+
+function passwordCompare($passwordOne, $passwordTwo) {
+    $result = strcmp($passwordOne, $passwordTwo);
+    return $result;
+}
 
 ?>
